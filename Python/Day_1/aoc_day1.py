141aeab50f67984f2353fa7399cbd5751cd121b2
Christian Graabæk Steenfeldt <c.graabaek@gmail.com>
2018-12-03 15:56:46 +0100

    Added Day 1, Part 2

--- Python/Day_1/aoc_day1.py
+++ Python/Day_1/aoc_day1.py
@@ -1,7 +1,20 @@
+import itertools as iter
 print("Advent Of Code - Day 1")
-
 
 PUZZLEINPUT = open('input.txt', 'r').read()
 
-changes = sum(int(n) for n in PUZZLEINPUT.split())
+data = [int(n) for n in PUZZLEINPUT.split()]
+changes = sum(data)
 print("Part 1: Frequency Changes: {}".format(changes))
+
+total_freq = 0 
+seen_set = {0}
+
+for n in iter.cycle(data):
+    total_freq += n
+    if total_freq in seen_set:
+        print("Part 2: First duplicate: {}".format(total_freq))
+        break
+    else:
+        seen_set.add(total_freq)
+        
